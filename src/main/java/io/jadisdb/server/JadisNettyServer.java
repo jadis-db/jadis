3c966a0dbb83d9b6999e777d438bcbfd30b9b71d
inhochoi <revaer@naver.com>
2020-06-17 22:08:27 +0900

    add get message

--- src/main/java/io/jadisdb/server/JadisNettyServer.java
+++ src/main/java/io/jadisdb/server/JadisNettyServer.java
@@ -1,7 +1,15 @@
 package io.jadisdb.server;
 
+import com.linecorp.armeria.common.HttpResponse;
+import com.linecorp.armeria.server.Server;
+import com.linecorp.armeria.server.ServerBuilder;
+import com.linecorp.armeria.server.annotation.Get;
+import com.linecorp.armeria.server.annotation.Param;
 import io.jadisdb.JadisConfig;
 import io.jadisdb.dataaccess.DataAccess;
+
+import java.util.concurrent.CompletableFuture;
+
 
 public class JadisNettyServer implements JadisServer {
     private DataAccess dataAccess;
@@ -12,6 +20,19 @@ public class JadisNettyServer implements JadisServer {
 
     @Override
     public void runServer(JadisConfig jadisConfig) {
+        ServerBuilder sb = Server.builder();
+        sb.http(jadisConfig.getPort());
 
+        sb.annotatedService(new Object() {
+            @Get("/data/:key")
+            public HttpResponse get(@Param("key") String key) {
+                return HttpResponse.of(dataAccess.get(key) == null ? "null" : dataAccess.get(key));
+            }
+        });
+
+
+        Server server = sb.build();
+        CompletableFuture<Void> future = server.start();
+        future.join();
     }
 }
